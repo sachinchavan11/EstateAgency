93755e61f8d26a452e80188e97111b826c674162
Sachin <lavudyasachinchavan@gmail.com>
2023-03-30 17:38:54 +0530

    changed title in every page

--- forms/contact.php
+++ forms/contact.php
@@ -6,8 +6,8 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace dreamcasahyderabad@gmail.com with your real receiving email address
-  $receiving_email_address = 'dreamcasahyderabad@gmail.com';
+  // Replace dreamcasarealestate@gmail.com with your real receiving email address
+  $receiving_email_address = 'dreamcasarealestate@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
