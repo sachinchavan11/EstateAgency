47b2061fe256eadccaabef826b5f7db27ea8d337
Sachin <lavudyasachinchavan@gmail.com>
2023-03-31 13:16:57 +0530

    added phone feature in  navbar and commented property feature from navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,18 +80,18 @@
   /**
    * Search window open/close
    */
-  let body = select('body');
-  on('click', '.navbar-toggle-box', function(e) {
-    e.preventDefault()
-    body.classList.add('box-collapse-open')
-    body.classList.remove('box-collapse-closed')
-  })
+  // let body = select('body');
+  // on('click', '.navbar-toggle-box', function(e) {
+  //   e.preventDefault()
+  //   body.classList.add('box-collapse-open')
+  //   body.classList.remove('box-collapse-closed')
+  // })
 
-  on('click', '.close-box-collapse', function(e) {
-    e.preventDefault()
-    body.classList.remove('box-collapse-open')
-    body.classList.add('box-collapse-closed')
-  })
+  // on('click', '.close-box-collapse', function(e) {
+  //   e.preventDefault()
+  //   body.classList.remove('box-collapse-open')
+  //   body.classList.add('box-collapse-closed')
+  // })
 
   /**
    * Intro Carousel
